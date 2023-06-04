5332d019100d29388419fb5b1f5052e64ad44261
unknown <8268363@gmail.com>
2023-06-04 13:20:18 +0300

    update ymaps script

--- routes/contacts.tsx
+++ routes/contacts.tsx
@@ -6,7 +6,9 @@ import MyFooter from "../islands/myFooter.tsx";
 export default function Contacts() {
   function startYMaps() {
     // @ts-ignore: ymaps
-    ymaps.ready(function () {
+    ymaps.ready(init);
+
+    function init() {
       // @ts-ignore: ymaps
       const myMap = new ymaps.Map("map", {
           center: [53.912845, 27.561094],
@@ -14,33 +16,26 @@ export default function Contacts() {
         }, {
           searchControlProvider: "yandex#search",
         }),
-        // Создаём макет содержимого.
         // @ts-ignore: ymaps
         MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
           '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>',
         ),
         // @ts-ignore: ymaps
+
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
           hintContent: "Специализированная юридическая консультация",
           balloonContent: null,
         }, {
-          // Опции.
-          // Необходимо указать данный тип макета.
           iconLayout: "default#image",
-          // Своё изображение иконки метки.
           iconImageHref: "../media/contacts/ymaps_target.png",
-          // Размеры метки.
           iconImageSize: [30, 40],
-          // Смещение левого верхнего угла иконки относительно
-          // её "ножки" (точки привязки).
           iconImageOffset: [-5, -38],
         });
 
       myMap.geoObjects
         .add(myPlacemark)
-        // @ts-ignore: ymaps
-        .add(myPlacemarkWithContent);
-    });
+        .add(MyIconContentLayout);
+    }
   }
 
   useEffect(() => {
